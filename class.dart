6a91feb90a1ce2392b221790a1d70e194f8d93bc
AqsaBano330 <aqsabano08@gmail.com>
2023-06-16 20:59:14 +0500

    oprators

--- class.dart
+++ class.dart
@@ -1,57 +1,68 @@
 void main() {
-  int num = 15;
+  int a = 10;
+  int b = 5;
 
-  if (num > 10) {
-    print("Number is greater than 10.");
+  // Arithmetic Operators
+  int sum = a + b;
+  int difference = a - b;
+  int product = a * b;
+  double quotient = a / b;
+  int remainder = a % b;
 
-    if (num < 20) {
-      print("Number is less than 20.");
+  print("Arithmetic Operators:");
+  print("Sum: $sum");
+  print("Difference: $difference");
+  print("Product: $product");
+  print("Quotient: $quotient");
+  print("Remainder: $remainder");
 
-      if (num % 2 == 0) {
-        print("Number is even.");
+  // Relational Operators
+  bool isEqual = a == b;
+  bool isNotEqual = a != b;
+  bool isGreater = a > b;
+  bool isLess = a < b;
+  bool isGreaterOrEqual = a >= b;
+  bool isLessOrEqual = a <= b;
 
-        if (num > 12) {
-          print("Number is greater than 12.");
+  print("\nRelational Operators:");
+  print("Is Equal: $isEqual");
+  print("Is Not Equal: $isNotEqual");
+  print("Is Greater: $isGreater");
+  print("Is Less: $isLess");
+  print("Is Greater or Equal: $isGreaterOrEqual");
+  print("Is Less or Equal: $isLessOrEqual");
 
-          if (num < 18) {
-            print("Number is less than 18.");
+  // Logical Operators
+  bool logicalAnd = (a > 0) && (b > 0);
+  bool logicalOr = (a > 0) || (b > 0);
+  bool logicalNotA = !(a > 0);
+  bool logicalNotB = !(b > 0);
 
-            if (num % 3 == 0) {
-              print("Number is divisible by 3.");
+  print("\nLogical Operators:");
+  print("Logical AND: $logicalAnd");
+  print("Logical OR: $logicalOr");
+  print("Logical NOT for A: $logicalNotA");
+  print("Logical NOT for B: $logicalNotB");
 
-              if (num > 14) {
-                print("Number is greater than 14.");
+  // Assignment Operators
+  int x = 3;
+  x += 2;
+  int y = 7;
+  y -= 3;
 
-                if (num < 16) {
-                  print("Number is less than 16.");
+  print("\nAssignment Operators:");
+  print("Value of x: $x");
+  print("Value of y: $y");
 
-                  if (num % 5 == 0) {
-                    print("Number is divisible by 5.");
-                  } else {
-                    print("Number is not divisible by 5.");
-                  }
-                } else {
-                  print("Number is not less than 16.");
-                }
-              } else {
-                print("Number is not greater than 14.");
-              }
-            } else {
-              print("Number is not divisible by 3.");
-            }
-          } else {
-            print("Number is not less than 18.");
-          }
-        } else {
-          print("Number is not greater than 12.");
-        }
-      } else {
-        print("Number is not even.");
-      }
-    } else {
-      print("Number is not less than 20.");
-    }
-  } else {
-    print("Number is not greater than 10.");
-  }
+  // Ternary Operator
+  int maxNumber = (a > b) ? a : b;
+
+  print("\nTernary Operator:");
+  print("Maximum Number: $maxNumber");
+
+  // Conditional Expression
+  String result = (a % 2 == 0) ? "Even" : "Odd";
+
+  print("\nConditional Expression:");
+  print("Result: $result");
 }
